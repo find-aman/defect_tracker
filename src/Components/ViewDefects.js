0206f6b1a070f2a3f4e1d3f229e462de2de8984b
Aman Gupta <amang9578@gmail.com>
2021-01-16 21:17:11 +0530

    ViewDefects components created

---
+++ src/Components/ViewDefects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ViewDefects extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Defect Tracker</h1>
+            </div >
+        )
+    }
+
+}
+
+export default ViewDefects
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ViewDefects from './Components/ViewDefects'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ViewDefects />
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
